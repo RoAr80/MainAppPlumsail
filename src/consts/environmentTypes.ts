9b84b540cd5e8983f5069d05d3fbb1e0e4db37c5
RoAr80 <arabov.r.r@mail.ru>
2023-05-24 12:56:28 +0300

    add prod main

--- src/consts/environmentTypes.ts
+++ src/consts/environmentTypes.ts
@@ -2,3 +2,4 @@ export const LOCAL_ENV = 'local';
 export const DEVELOPMENT_ENV = 'development';
 export const PRODUCTION_ENV = 'production';
 export const PRODUCTION_ENV2 = 'production2';
+export const PRODUCTION_MAIN = 'productionMain';
