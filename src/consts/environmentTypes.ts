9d6991267d887eb255d95863981f2a232dd3436e
RoAr80 <arabov.r.r@mail.ru>
2023-05-24 13:12:19 +0300

    coolfeature_local11

--- src/consts/environmentTypes.ts
+++ src/consts/environmentTypes.ts
@@ -4,3 +4,4 @@ export const PRODUCTION_ENV = 'production';
 export const PRODUCTION_ENV2 = 'production2';
 export const PRODUCTION_MAIN = 'productionMain';
 export const PRODUCTION_MAIN3 = 'productionMain3';
+export const COOLFEATURE_MAIN3 = 'coolFeatureMain3';
