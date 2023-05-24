f98acb043a2ce184687701882182a3bbb5fb4958
RoAr80 <arabov.r.r@mail.ru>
2023-05-24 14:39:29 +0300

    newfeature_local1change

--- src/consts/environmentTypes.ts
+++ src/consts/environmentTypes.ts
@@ -6,3 +6,4 @@ export const PRODUCTION_MAIN = 'productionMain';
 export const PRODUCTION_MAIN3 = 'productionMain3';
 export const COOLFEATURE_MAIN3 = 'coolFeatureMain3';
 export const MAIN_CHANGE = 'mainChange1';
+export const NEWFEATURE_LOCAL1CHANGE = 'NEWFEATURE_LOCAL1CHANGE1';
